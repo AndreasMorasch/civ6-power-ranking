6628a344189523f1fd628e57ef86858e3e340487
Andreas Morasch <a.morasch@basecom.de>
2025-06-30 22:36:43 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wallpaper-container">
+  </div>
+
+  <video autoplay muted loop class="sparkles-video">
+    <source src="@/assets/sparkles.mp4" type="video/mp4">
+  </video>
+
+  <video autoplay muted loop class="smoke-video">
+    <source src="@/assets/smoke.mp4" type="video/mp4">
+  </video>
+</template>
